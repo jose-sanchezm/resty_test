10c40cbcaa4cb102378f51e10963dc705237f411
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2019-11-13 11:19:23 +0100

    Add test files

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/go-resty/resty/v2"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Debugf("Sending request without logrus")
+	resty.New().SetDebug(true).R().Get("www.google.com")
+	logrus.Debugf("Request sent")
+
+	logrus.Debugf("Sending request with logrus")
+	resty.New().SetLogger(logrus.New()).SetDebug(true).R().Get("www.google.com")
+	logrus.Debugf("Request sent")
+}
